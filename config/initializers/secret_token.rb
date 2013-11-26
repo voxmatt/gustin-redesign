81270f652f74244047f7bffa9ec1b32cb80fe138
voxmatt <public@matthewmartin.com>
2013-11-26 12:56:31 -0800

    homepage redesign

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gustin::Application.config.secret_key_base = '06e022bdb5fdfd8f4d7a08955f635c55cc68b7b552211cab177d5577df39774e78170b5812e99f6f0570631cad5b02e623ec9cd88f2f35964a5dfed532fdaff4'
